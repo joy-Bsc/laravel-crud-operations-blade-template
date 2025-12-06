9724e8c5d3af3e7df0cc7eac6dc52ddf2bca01af
Joy Dhar <dharjoy75@gmail.com>
2025-12-06 18:43:02 +0600

    initialize project

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+<template>
+    <Head title="About" />
+    <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <h1 class="text-6xl font-bold text-gray-900 dark:text-white">About Us</h1>
+    </div>
+</template>
---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+
+withDefaults(
+    defineProps<{
+        canRegister: boolean;
+    }>(),
+    {
+        canRegister: true,
+    },
+);
+</script>
+
+<template>
+    <Head title="Hello" />
+    <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <h1 class="text-6xl font-bold text-gray-900 dark:text-white">Home</h1>
+    </div>
+</template>
+
+
