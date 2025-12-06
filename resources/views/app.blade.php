29ca3bbef043140765a51f51b8b1c5e08dbc10a4
Joy Dhar <dharjoy75@gmail.com>
2025-12-06 20:18:14 +0600

    refactor: Disable Vite and Inertia for a plain Blade SPA implementation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,10 +39,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
-        @inertiaHead
+        {{-- Vite disabled - using plain Blade SPA instead --}}
     </head>
     <body class="font-sans antialiased">
-        @inertia
+        {{-- Inertia disabled - not using Inertia anymore --}}
     </body>
 </html>
